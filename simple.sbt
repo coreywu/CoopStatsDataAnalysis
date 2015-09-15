78748a206a161de1e8cd970672455f757e25f788
Corey Wu <coreywu0@gmail.com>
2015-09-15 00:27:47 -0400

    Complete Spark Random Walk

--- simple.sbt
+++ simple.sbt
@@ -2,7 +2,7 @@ name := "Simple Project"
 
 version := "1.0"
 
-scalaVersion := "2.11.7"
+scalaVersion := "2.10.5"
 
 EclipseKeys.withSource := true
 
--- src/main/scala/com/corey/algorithms/spark/SparkRandomWalk.scala
+++ src/main/scala/com/corey/algorithms/spark/SparkRandomWalk.scala
@@ -1,6 +1,7 @@
 package com.corey.algorithms
 
 import scala.collection.immutable.HashMap
+import scala.collection.immutable.Map
 import scala.collection.mutable.Buffer
 import scala.io.Source
 import scala.collection.immutable.HashSet
@@ -11,24 +12,29 @@ import org.apache.spark.HashPartitioner
 import com.corey.algorithms.PageRank._
 import scala.util.Random
 import org.apache.spark.AccumulatorParam
+import org.apache.spark.Accumulator
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
 
 object SparkRandomWalk {
   
   type CompanyOccurrences = Map[String, Int]
   
-  class CompanyOccurrencesPerTermAP extends AccumulatorParam[Array[CompanyOccurrences]] {
-    def zero(array: Array[CompanyOccurrences]) = new Array(6)
+  class CompanyOccurrencesAP extends AccumulatorParam[CompanyOccurrences] {
+    def zero(companyOccurrences: CompanyOccurrences) = HashMap()
     
-    def addInPlace(array1: Array[CompanyOccurrences], array2: Array[CompanyOccurrences]) = {
-      for (i <- 0 until 6) {
-        for ((company, occurrences2) <- array2(i)) {
-          if (array1(i).contains(company)) {
-            val occurrences1 = array1(i)(company)
-            array1(i) += ((company, occurrences1 + occurrences2))
-          }
+    def addInPlace(companyOccurrences1: CompanyOccurrences, companyOccurrences2: CompanyOccurrences) = {
+      var newCompanyOccurrences = HashMap() ++ companyOccurrences1
+    	for ((company, occurrences2) <- companyOccurrences2) {
+    		if (companyOccurrences1.contains(company)) {
+    			val occurrences1 = companyOccurrences1(company)
+    		  newCompanyOccurrences += ((company, occurrences1 + occurrences2))
+    		} else {
+    		  newCompanyOccurrences += ((company, occurrences2))
         }
-      }
-      array1
+    	}
+      newCompanyOccurrences
     }
   }
   
@@ -58,60 +64,58 @@ object SparkRandomWalk {
     
     val coopsRDD: RDD[Buffer[String]] = sc.parallelize(coops).persist()
     
-//    var firstCompanyRDD: RDD[String] = 
-//    var nextCoopRDD: RDD[(String, String)] = List()
-
     val firstCompanyRDD: RDD[String] = for {
       companyNames <- coopsRDD
       start = if (companyNames.length > 6) (companyNames.length - 6) else 0
     } yield companyNames(start)
 
-    val nextCoopRDD: RDD[(String, String)] = for {companyNames <- coopsRDD
-      val start = if (companyNames.length > 6) (companyNames.length - 6) else 0
+    val nextCoopRDD: RDD[(String, String)] = for {
+      companyNames <- coopsRDD
+      start = if (companyNames.length > 6) (companyNames.length - 6) else 0
       i <- start until companyNames.length - 1
       firstCompanyName = companyNames(i)
       secondCompanyName = companyNames(i + 1) 
       term = i - start
     } yield (firstCompanyName, secondCompanyName)
     
-//    val firstCompanyRDD: RDD[String] = sc.parallelize(firstCompanyList).persist()
-//    val nextCoopRDD: RDD[(String, String)] = sc.parallelize(nextCoopList).persist()
-    
-    val nextCoopMapRDD: RDD[(String, Iterable[String])] = nextCoopRDD.groupBy(_._1).map {
+    val nextCoopMap: Map[String, Iterable[String]] = nextCoopRDD.groupBy(_._1).map {
       case (companyName, tupleList) => (companyName, tupleList.map(_._2))
-    }
+    }.collect.toMap
 
-    val companiesForEachTermRDD: RDD[List[String]] = sc.parallelize(Array.fill(6)(List()))
-    
     val iterationsOverCountFraction = iterations / firstCompanyRDD.count
     val randomFirstCompaniesRDD = firstCompanyRDD.sample(true, iterationsOverCountFraction)
+
+    val occurrencesPerTerm: Array[Accumulator[CompanyOccurrences]] = Array.fill(6)(sc.accumulator[CompanyOccurrences](Map[String, Int]())(new CompanyOccurrencesAP))
     
-//    companiesForEachTermRDD.zipWithIndex().map {
-//      case (companiesList, termIndex) => if (termIndex > 0) {
-//    	  for {
-//    		  company <- companiesForEachTermRDD.
-//
-//    		  term <- 1 until 6
-//    		  if (nextCoopMapRDD.lookup(company).size > 0) {
-//    			  company = nextCoopMapRDD.lookup(company)(0).drop(Random.nextInt(nextCoopMapRDD.lookup(company).length)).iterator.next()
-//    					  companiesForEachTerm(term) +:= company
-//    		  }
-//    	  } yield company
-//      }
-//    }
+    for (randomFirstCompany <- randomFirstCompaniesRDD) {
+      var company = randomFirstCompany
+      occurrencesPerTerm(0) += Map(company -> 1)
+      
+      for (term <- 1 until 6) {
+        if (nextCoopMap.contains(company)) {
+          company = nextCoopMap(company).drop(Random.nextInt(nextCoopMap(company).size)).iterator.next()
+          occurrencesPerTerm(term) += Map(company -> 1)
+        }
+      }
+    }
     
-//    println(companiesForEachTerm.mkString("\n"))
-//    println()
+    val sortedCompanyOccurrencesPerTerm: Array[List[String]] = occurrencesPerTerm.map {
+      accumulator: Accumulator[Map[String, Int]] => accumulator.value.toList.sortBy((tuple: (String, Int)) => tuple._2)(Ordering.Int).map(_._1).reverse
+    }
+
+    val stringBuilder = new StringBuilder()
     
-//    val companyOccurencesForEachTerm: Array[Map[String, Int]] = companiesForEachTerm.map {
-//      _.groupBy(identity).mapValues(_.size)
-//    }
-//    
-//    val sortedCompanyOccurencesForEachTerm: Array[List[String]] = companyOccurencesForEachTerm.map {
-//      map: Map[String, Int] => map.toList.sortBy((tuple: (String, Int)) => tuple._2)(Ordering.Int).map(_._1).reverse
-//    }
-//    
-//    println(sortedCompanyOccurencesForEachTerm.mkString("\n"))
+    for (i <- 1 to 6) {
+      stringBuilder.append("Term " + i + ": ")
+      stringBuilder.append(sortedCompanyOccurrencesPerTerm(i - 1).take(10).mkString(", "))
+      stringBuilder.append("\n")
+    }
+    
+    println(stringBuilder.toString())
+
+    val bufferedWriter = new BufferedWriter(new FileWriter(new File("spark_results/random_walk_spark")))
+    bufferedWriter.write(stringBuilder.toString())
+    bufferedWriter.close()
   }
   
 }
